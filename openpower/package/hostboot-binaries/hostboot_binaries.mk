33c752404841f523c2a24272badf2c7abf0fd110
Dean Sanner <dsanner@us.ibm.com>
2017-08-03 16:30:00 -0500

    Number of user level changes to enable WSP MFG
      -Updated XML for Obus bricks
      -Updated HCODE (inits/perf)
      -Updated HB to work with newer HCODE
      -Updated skiboot to lastest release (pick up Alistair's commits)

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOSTBOOT_BINARIES_VERSION ?= c24fccd3a4b413968ea02dfaeb6f23be8f749f67
+HOSTBOOT_BINARIES_VERSION ?= 14549b49f12be896942c87f34cf18d82b6063520
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
