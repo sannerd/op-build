83d1646780718fb02411145f653df6f00b5f5e58
Dean Sanner <dsanner@us.ibm.com>
2017-10-22 06:29:56 -0500

    Urgent fixes for 1742C driver
     -Backleveled HB commit to avoid the Scom multicast fix
     -Added patches to HB for:
       128GB dimm fails (https://ralgit01.raleigh.ibm.com/gerrit1/#/c/47173/)
       Disable resclock pulsed mode (https://ralgit01.raleigh.ibm.com/gerrit1/#/c/48777/)
     -Backleveled OCC level to avoid MFG mode boot fail

--- openpower/package/occ/occ.mk
+++ openpower/package/occ/occ.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OCC_VERSION_BRANCH_MASTER_P8 ?= 28f2cec690b7f19548ce860a8820f519e6c39a6a
-OCC_VERSION_BRANCH_MASTER ?= 6404302e1b1eae001edcec4a5c8e4c4ce5cdaaa3
+OCC_VERSION_BRANCH_MASTER ?= cff91e53cc1a8324654b52d63a2620045c2b0b32
 
 OCC_VERSION ?= $(if $(BR2_OPENPOWER_POWER9),$(OCC_VERSION_BRANCH_MASTER),$(OCC_VERSION_BRANCH_MASTER_P8))
 OCC_SITE ?= $(call github,open-power,occ,$(OCC_VERSION))
